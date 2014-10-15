d6a58cdf9bf47cbe2dcae0ee0299653051ed1dd0
sunnyvirk <sunny.virk@mac.com>
2014-10-15 14:13:35 +0100

    grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,105 @@
+'use strict';
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        env: {
+            dev: {
+                NODE_ENV: 'development'
+            },
+            test: {
+                NODE_ENV: 'test'
+            }
+        },
+        nodemon: {
+            dev: {
+                script: 'server.js',
+                options: {
+                    ext: 'js, html',
+                    watch: ['server.js', 'config/**/*.js', 'app/**/*.js']
+                }
+            },
+            debug: {
+                script: 'server.js',
+                options: {
+                    nodeArgs: ['--debug'],
+                    ext: 'js, html',
+                    watch: ['server.js', 'config/**/*.js', 'app/**/*.js']
+                }
+            }
+        },
+        mochaTest: {
+            src: 'app/tests/**/*.js',
+            options: {
+                reporter: 'spec'
+            }
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js'
+            }
+        },
+        protractor: {
+            e2e: {
+                options: {
+                    configFile: 'protractor.conf.js'
+                }
+            }
+        },
+        jshint: {
+            options: {
+                node: true
+            },
+            all: {
+                src: ['server.js', 'config/**/*.js', 'app/**/*.js', 'public/js/*.js', 'public/modules/**/*.js']
+            }
+        },
+        csslint: {
+            all: {
+                src: 'public/modules/**/*.css'
+            }
+        },
+        watch: {
+            js: {
+                files: ['server.js', 'config/**/*.js', 'app/**/*.js', 'public/js/*.js', 'public/modules/**/*.js'],
+                tasks: ['jshint']
+            },
+            css: {
+                files: 'public/modules/**/*.css',
+                tasks: ['csslint']
+            }
+        },
+        concurrent: {
+            dev: {
+                tasks: ['nodemon', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            },
+            debug: {
+                tasks: ['nodemon:debug', 'watch', 'node-inspector'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        },
+        'node-inspector': {
+            debug: {}
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-env');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-protractor-runner');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-node-inspector');
+
+    grunt.registerTask('default', ['env:dev', 'concurrent:dev']);
+    grunt.registerTask('debug', ['env:dev', 'concurrent:debug']);
+    grunt.registerTask('test', ['env:test', 'mochaTest', 'karma', 'protractor']);
+    grunt.registerTask('lint', ['jshint', 'csslint']);
+};
